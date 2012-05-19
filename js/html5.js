4d275c95d0286929525749abccbea76b89e397bc
Jim Kersey <jim.kersey@gmail.com>
2012-05-19 05:58:13 -0500

    more good fixes

--- js/html5.js
+++ js/html5.js
@@ -393,6 +393,39 @@ function get_target(id) {
     }
     return 0;
 }
+function pixel_to_tile(x, y) {
+	"use strict";
+
+    var 
+	tile,
+	x_tile = x >> 5,
+    y_tile = y >> 5;
+
+    try {
+		tile = map[y_tile][x_tile];
+		if(tile > 47) {
+			return map[y_tile][x_tile];
+		} else {
+			return 0;
+		}
+    } catch(err) {
+        return 0;
+    }
+}
+
+function intersectedTile(entity) {
+	"use strict";
+    return pixel_to_tile(entity.x, entity.y) > 0;
+}
+
+
+function true_pixel_to_tile(x, y) {
+	"use strict";
+    var 
+	x_tile = x >> 5,
+    y_tile = y >> 5;
+    return map[y_tile][x_tile];
+}
 
 function map_iterate(func) {
 	"use strict";
@@ -635,6 +668,100 @@ function build_door(x_t, y_t, tp, img, tile, key) {
 
     return entity;
 
+}
+
+function build_jetpack(x_t, y_t, tp, img, tile, key) {
+	"use strict";
+
+    var entity = new Entity(x_t, y_t, tp, img, tile, key);
+
+    entity.check_player = function() {
+        if(intersect(player.x, player.y+32,32, 32, this.x, this.y, 32, 32 )) {
+            this.alive = 0;
+            player.has_jetpack = true;
+            player.jetpack_fuel = 200;
+        }
+    };
+    return entity;
+
+}
+
+function build_crate(x_t, y_t, tp, img, tile, key) {
+	"use strict";
+
+    var entity = new Entity(x_t, y_t, tp, img, tile, key);
+    entity.is_teleportable = true;
+
+    entity.check_player = function() {
+        if(!player.grounded && player.y + 32 < this.y) {
+            if(player.y > this.y - 48) {
+                player.y = this.y - 48;
+            }
+            player.on_a_crate = true;
+        }
+        if(player.grounded && !player.on_a_crate){
+            if (this.x - 15 > player.x) {
+                if(player.grounded) {
+                    //this.x += 2;
+                    if(pixel_to_tile(this.x + 32, this.y) > 0) {
+                        //this.x -= 2;
+                        player.x-=4;
+                    }
+                }
+                player.x-=4;
+            } else if (this.x + 15 < player.x) {
+                if(player.grounded) {
+                    //this.x -= 2;
+                    if(pixel_to_tile(this.x, this.y) > 0) {
+                        //this.x += 2;
+                        player.x+=4;
+                    }
+                }
+                player.x+=4;
+            }
+        }
+    };
+
+    entity.move = function() {
+        for(var i = 0; i < entities.length; ++i) {
+            if(intersect(this.x, this.y, 32, 32, entities[i].x, entities[i].y, 32, 23)) {
+                if(entities[i].type == T_CRATE) {
+                    if(this.y > entities[i].y) {
+                        entities.y--;
+                    }
+                    if(this.x > entities[i].x) {
+                        //this.x++;
+                        entities.x--;
+                    } else if(this.x < entities[i].x) {
+                        //this.x--;
+                        entities.x++;
+                    }
+
+                }
+            }
+        }
+
+        for(var k = 0; k < enemies.length; ++k) {
+            if(intersect(this.x, this.y, 32, 32, enemies[k].x, enemies[k].y + 8, 32, 48)) {
+                if(this.y_inertia > 1 && enemies[k].y_inertia == 1 && this.y < enemies[k].y) {
+                    enemies[k].alive = false;
+                    fire_particles(enemies[k].x, enemies[k].y, 4, 'red');
+                }
+            }
+        }
+        if(this.y_inertia < 10) {
+            this.y_inertia = this.y_inertia * 2;
+        } else {
+            this.y_inertia = 10;
+        }
+        this.y += this.y_inertia;
+        if(pixel_to_tile(this.x, this.y + 32) > 0 ||
+           pixel_to_tile(this.x + 32, this.y + 32) > 0) {
+            this.y -= this.y_inertia;
+            this.y_inertia = 1;
+        }
+    };
+    return entity;
 }
 
 
@@ -1892,82 +2019,6 @@ function build_person(x_t, y_t, tp, img, tile, key) {
 //////////////////////////////////////////////////////////////////
 //  CRATE
 
-function build_crate(x_t, y_t, tp, img, tile, key) {
-
-    var entity = new Entity(x_t, y_t, tp, img, tile, key);
-    entity.is_teleportable = true;
-
-    entity.check_player = function() {
-        if(!player.grounded && player.y + 32 < this.y) {
-            if(player.y > this.y - 48) {
-                player.y = this.y - 48;
-            }
-            player.on_a_crate = true;
-        }
-        if(player.grounded && !player.on_a_crate){
-            if (this.x - 15 > player.x) {
-                if(player.grounded) {
-                    //this.x += 2;
-                    if(pixel_to_tile(this.x + 32, this.y) > 0) {
-                        //this.x -= 2;
-                        player.x-=4;
-                    }
-                }
-                player.x-=4;
-            } else if (this.x + 15 < player.x) {
-                if(player.grounded) {
-                    //this.x -= 2;
-                    if(pixel_to_tile(this.x, this.y) > 0) {
-                        //this.x += 2;
-                        player.x+=4;
-                    }
-                }
-                player.x+=4;
-            }
-        }
-    };
-
-    entity.move = function() {
-        for(var i = 0; i < entities.length; ++i) {
-            if(intersect(this.x, this.y, 32, 32, entities[i].x, entities[i].y, 32, 23)) {
-                if(entities[i].type == T_CRATE) {
-                    if(this.y > entities[i].y) {
-                        entities.y--;
-                    }
-                    if(this.x > entities[i].x) {
-                        //this.x++;
-                        entities.x--;
-                    } else if(this.x < entities[i].x) {
-                        //this.x--;
-                        entities.x++;
-                    }
-
-                }
-            }
-        }
-
-        for(var k = 0; k < enemies.length; ++k) {
-            if(intersect(this.x, this.y, 32, 32, enemies[k].x, enemies[k].y + 8, 32, 48)) {
-                if(this.y_inertia > 1 && enemies[k].y_inertia == 1 && this.y < enemies[k].y) {
-                    enemies[k].alive = false;
-                    fire_particles(enemies[k].x, enemies[k].y, 4, 'red');
-                }
-            }
-        }
-        if(this.y_inertia < 10) {
-            this.y_inertia = this.y_inertia * 2;
-        } else {
-            this.y_inertia = 10;
-        }
-        this.y += this.y_inertia;
-        if(pixel_to_tile(this.x, this.y + 32) > 0 ||
-           pixel_to_tile(this.x + 32, this.y + 32) > 0) {
-            this.y -= this.y_inertia;
-            this.y_inertia = 1;
-        }
-    };
-    return entity;
-}
 
 //////////////////////////////////////////////////////////////////
 //  SWITCH
@@ -2026,19 +2077,6 @@ function build_exit(x_t, y_t, tp, img, tile, key) {
 //////////////////////////////////////////////////////////////////
 //  JETPACK
 
-function build_jetpack(x_t, y_t, tp, img, tile, key) {
-    entity = new Entity(x_t, y_t, tp, img, tile, key);
-
-    entity.check_player = function() {
-        if(intersect(player.x, player.y+32,32, 32, this.x, this.y, 32, 32 )) {
-            this.alive = 0;
-            player.has_jetpack = true;
-            player.jetpack_fuel = 200;
-        }
-    };
-    return entity;
-
-}
 
 //////////////////////////////////////////////////////////////////
 //  TELEPORTER
@@ -2085,30 +2123,6 @@ function draw_particles() {
 
 
 
-function intersectedTile(entity) {
-    return pixel_to_tile(entity['x'], entity['y']) > 0;
-}
-
-function pixel_to_tile(x, y) {
-    var x_tile = x >> 5;
-    var y_tile = y >> 5;
-    try {
-		var tile = map[y_tile][x_tile];
-		if(tile > 47) {
-			return map[y_tile][x_tile];
-		} else {
-			return 0;
-		}
-    } catch(err) {
-        return 0;
-    }
-}
-
-function true_pixel_to_tile(x, y) {
-    var x_tile = x >> 5;
-    var y_tile = y >> 5;
-    return map[y_tile][x_tile];
-}
 
 
 function resetting_level() {
