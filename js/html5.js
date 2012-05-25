8ead8b8d2ad1ce078b072942e45a88ddb8422bc7
Jim Kersey <jim.kersey@gmail.com>
2012-05-25 17:12:43 -0500

    adding version number, displaying on splash screen

--- js/html5.js
+++ js/html5.js
@@ -1,6 +1,6 @@
 /*global clearInterval: false, clearTimeout: false, document: false, event: false, frames: false, history: false, Image: false, location: false, name: false, navigator: false, Option: false, parent: false, screen: false, setInterval: false, setTimeout: false, window: false, XMLHttpRequest: false, onevar: false, undef: true, sloppy: true, stupid: true, vars: true */
 
-// v0.1.0
+var version = "v0.1.0";
 
 var god_mode = true;
 var images_loaded = 0;
@@ -1525,6 +1525,8 @@ function SplashScreen() {
     this.draw = function() {
 		var i;
         ctx.drawImage(splash_screen_img, 0, 0);
+		draw_text("ENGINE " + version, 2, 310);
+
         for(i = 0; i < this.buttons.length; ++i) {
             this.buttons[i].draw();
         }
