1198b08932de48b24337d7b5e392c32812f8afc5
Jim Kersey <jim.kersey@gmail.com>
2023-07-22 15:08:13 -0500

    testing permissions

--- publish.sh
+++ publish.sh
@@ -12,7 +12,5 @@ cp js/game_lib.js export/js/
 cp js/factories.js export/js/
 
 closure-compiler --compilation_level ADVANCED_OPTIMIZATIONS --js js/html5.js js/game_lib.js js/factories.js > export/js/html5_min.js
-# closure-compiler --compilation_level ADVANCED_OPTIMIZATIONS --js js/editor.js > export/js/editor_min.js
 
-#scp -r export/* root@scoab.com:/var/www/html/play/doctor-robot/
 cp -r export/* /var/www/html/doctor_robot/
